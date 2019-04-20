e188fa989b81330c9727fcad3f440c0b493fa60d
Rodrigo Felix <rodrigo.felix321@gmail.com>
2019-04-20 23:12:40 +0000

    modificacoes adiciona-produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -3,9 +3,19 @@
 <?php
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
+$conexao = mysqli_connect('localhost', 'root', 'root', 'loja');
+
+$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+
+if(mysqli_query($conexao, $query)) {
 ?>
-<p class="alert-success">
-    Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
-</p>
+    <p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php
+} else {
+?>
+    <p class="alert-danger">O produto <?= $nome; ?> não foi adicionado</p>
+<?php
+}
+?>
 
 <?php include("rodape.php"); ?>
